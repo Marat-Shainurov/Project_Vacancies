8b7b29ae598401134e107ebe8f6d051d95c340ae
Marat-Shainurov <m_shainurov@mail.ru>
2023-03-30 13:23:09 +0300

    main logic of the program in main.py added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,34 @@
-# Задача: нужно написать парсер для сайта, который собирает текущие вакансии в России для Python-разработчиков,
-# а также составить документ для возможных откликов.
+from src.classes import HH, SuperJob
+from src.jobs_classes import HHVacancy, SJVacancy, CountMixin
+from src.utils import get_top, sorting
 
-from src.classes import Engine, HH, SuperJob
-from src.connector import Connector
+# Создание объектов классов HH SuperJob.
+# По умолчанию поиск вакансий производится в России, по запросу "Python разработчик developer", без опыта работы.
+# При необходимости можно передать экземпляру значения атрибутам "key_text", "area", "experience". Детали в README.
+object_HH = HH()
+object_SJ = SuperJob()
+
+# Вызов методов классов HH SuperJob, для сбора и записи в общий файл response_data_json вакансий.
+object_HH.pass_to_insert_hh()
+object_SJ.pass_to_insert_sj()
+
+# Создаются классы Vacancy.
+object_HHV = HHVacancy()
+object_SJV = SJVacancy()
+
+# Запрос необходимое кол-ва ТОП вакансий по ЗП
+top_count = int(input("Введите необходимое кол-во вакансий"))
+
+sorted_array = sorting('response_data.json')
+top_to_apply = get_top('response_data.json', top_count)
+
+# Создаются экземпляры классов Vacancy.
+# Вызываются необходимые методы классов Vacancy для записи в файл потенциальных вакансий для отклика.
+for vacancy in top_to_apply:
+    obj_hh = HHVacancy()
+    obj_sj = SJVacancy()
+    obj_hh.write_to_file()
+    obj_sj.write_to_file()
+
+print(CountMixin.get_count_of_vacancy)
+print("Вакансии для потенциальных откликов записаны в файл 'best_vacancies.json'. Удачи на интервью!")

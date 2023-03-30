94f3af447c31680f872d2d531d845f0f3f55e1e3
Marat-Shainurov <m_shainurov@mail.ru>
2023-03-30 13:25:01 +0300

    mixin class updated, utils cleaned

--- src/jobs_classes.py
+++ src/jobs_classes.py
@@ -2,6 +2,7 @@ class Vacancy:
     __slots__ = ("vacancy_name", "vacancy_url", "vacancy_description", "vacancy_salary")
 
     def __init__(self):
+        super().__init__()
         self.vacancy_name = None
         self.vacancy_url = None
         self.vacancy_description = None
@@ -15,6 +16,15 @@ class Vacancy:
 
 
 class CountMixin:
+    """Класс Mixin для подсчета вакансий ресурсов HH и SJ."""
+
+    # Счетчики выданных вакансий
+    count_HH = 0
+    count_SJ = 0
+
+    def __init__(self):
+        CountMixin.count_SJ += 1
+        CountMixin.count_HH += 1
 
     @property
     def get_count_of_vacancy(self):
@@ -22,9 +32,10 @@ class CountMixin:
         Вернуть количество вакансий от текущего сервиса.
         Получать количество необходимо динамически из файла.
         """
-        pass
+        return f"Вакансий HH - {CountMixin.count_HH}, Вакансий SJ - {CountMixin.count_SJ}."
 
-class HHVacancy(Vacancy):  # add counter mixin
+
+class HHVacancy(Vacancy, CountMixin):  # add counter mixin
     """ HeadHunter Vacancy """
 
     def __str__(self):
@@ -32,7 +43,7 @@ class HHVacancy(Vacancy):  # add counter mixin
 
 
 
-class SJVacancy(Vacancy):  # add counter mixin
+class SJVacancy(Vacancy, CountMixin):  # add counter mixin
     """ SuperJob Vacancy """
 
     def __str__(self):
--- src/utils.py
+++ src/utils.py
@@ -3,15 +3,6 @@ import requests
 import time
 
 
-def print_info(object_to_print) -> str:
-    """
-    Вспомогательный статичный метод, для поиска нужной инфо в коллекциях, для создания методов и атрибутов.
-    Печатает коллекцию в удобном для чтения формате.
-    """
-    res = json.dumps(object_to_print, indent=2, ensure_ascii=False)
-    return res
-
-
 def sorting(vacancies):
     """ Должен сортировать любой список вакансий по ежемесячной оплате (gt, lt magic methods) """
     pass
@@ -20,68 +11,3 @@ def sorting(vacancies):
 def get_top(vacancies, top_count):
     """ Должен возвращать {top_count} записей из вакансий по зарплате (iter, next magic methods) """
     pass
-
-
-# шаблон запросов к API HH and SuperJob
-
-def get_page(page=0):
-    """
-    Метод для получения страницы со списком вакансий
-    page - страницы начинается с 0 (с первой страницы).
-    """
-
-    # справочник для параметров запроса
-    params = {
-        'text': 'NAME:Python',  # текст фильтра, в имени должно быть Python
-        'area': 1,  # код локацииб 1 - Мск
-        'page': page,  # индекс страницы
-        'per_page': 100  # кол-во вакансий на 1 странице
-    }
-
-    req = requests.get('https://api.hh.ru/vacancies', params)  # Запрос к AIP
-    data = req.content.decode()  # Декодирует, чтобы кириллица отображалась корректно
-    req.close()
-
-    return data
-
-
-js_objs = []
-
-for page in range(0, 5):
-    js_obj = json.loads(get_page(page))  # Преобразовывает текст ответа в словарь Python
-    js_objs.extend(js_obj['items'])  # Добавляет текущий ответ в js_objs
-
-    if (js_obj['pages'] - page) <= 1:  # Проверка на последнюю страницу, если вакансий меньше 2000
-        break
-    time.sleep(0.25)
-
-count = 0
-for i in js_objs:
-    print(i)
-    count += 1
-
-
-def get_page_SJ(page=0):
-    params = {
-        'keyword': 'Python, Developer, Разработчик',
-        'town': 'Москва', 'page': page, 'count': 100}
-    headers = {
-        "X-Api-App-Id": "v3.r.137452619.a63bcb7404a9e35b86138b7522ea580731285cad.4636fdc6cb7aa59a9a3960e3a42fd0d53b449a72"}
-    req = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=headers, params=params)  # Запрос к AIP
-    data = req.content.decode()  # Декодирует, чтобы кириллица отображалась корректно
-    req.close()
-
-    return data
-
-
-js_objs_SJ = []
-
-for page in range(0, 3):
-    js_obj = json.loads(get_page_SJ(page))  # Преобразовывает текст ответа в словарь Python
-    js_objs_SJ.extend(js_obj['objects'])  # Добавляет текущий ответ в js_objs_SJ
-
-count = 0
-for i in js_objs_SJ:
-    print(print_info(i))
-    count += 1
-print(count)

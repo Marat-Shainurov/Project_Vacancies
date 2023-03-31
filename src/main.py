9abaf6a0d94afb43cfc685db6e75dda74ed8d4c4
Marat-Shainurov <m_shainurov@mail.ru>
2023-03-31 23:51:10 +0300

    main.py updated

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
-from src.classes import HH, SuperJob
 from src.jobs_classes import CountMixin
 from src.utils import get_top, write_to_file_final, get_started
 
 # Интерактив с пользователем, создание объектов классов HH SuperJob.
 objects = get_started()
 
-# Вызов методов классов HH и SuperJob, для сбора и записи в общий файл response_data_json вакансий.
+# Вызов методов классов HH и SuperJob, для сбора вакансий и записи в общий файл response_data_json.
 objects[0].pass_to_insert_hh()
 objects[1].pass_to_insert_sj()
 
-# Запрос необходимого для итоговой записи в vacancies_to_apply.txt кол-ва ТОП вакансий по ЗП.
+# Запрос необходимого кол-ва ТОП вакансий по ЗП, для итоговой записи в vacancies_to_apply.txt.
 top_count = int(input("Введите необходимое кол-во вакансий для записи в файл для откликов: "))
 top_to_apply = get_top('response_data.json', top_count)
 
 # Создаются экземпляры классов Vacancy, вызывается метод для записи в файл потенциальных вакансий для отклика.
 write_to_file_final(top_to_apply, f'../src/vacancies_to_apply.txt')
 
+# Завершение программы
 print(CountMixin().get_count_of_vacancy)
 print()
 print("Вакансии для потенциальных откликов записаны в файл 'vacancies_to_apply.txt'.\nУдачи на интервью! :)")
